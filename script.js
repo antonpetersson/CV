8d53e7c71448bcf5e557f7269d52d28adc304ba1
antonpetersson <antonpetersson@users.noreply.github.com>
2017-12-18 17:42:41 +0100

    Fixat infoboxar på sida 2.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-        $(".title").animate({marginLeft: "2vw"},{duration: 2000});
+    $(".title").animate({marginLeft: "2vw"},{duration: 2000});
 
-  
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $(".box1About").animate({marginLeft: "2vw"},{duration: 2000});
+        }
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            $(".box2About").animate({marginRight: "2vw"},{duration: 2000});
+        }
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 800) {
+            $(".box3About").animate({marginLeft: "2vw"},{duration: 2000});
+        }
+    });
+
+
 });
-    
+
